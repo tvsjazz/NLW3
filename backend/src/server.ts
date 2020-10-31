acfe7167ef1e34b56508ee4c14ef1d17b81b5484
Tiago Siqueira <tiagosiqueira@MacBook-Pro-de-Julia.local>
2020-10-31 15:03:06 -0300

    configuracao sqlite e criando orfanato sem imagem

---
+++ backend/src/server.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanages from './models/Orphanage';
+
+import './database/connection';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/orphanages', async (request, response) => {
+    const {
+        name,
+        latitude,
+        longetude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longetude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return response.json({ message: 'Hello World' });
+});
+
+app.listen(3333);
